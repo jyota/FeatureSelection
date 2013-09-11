b278ae8dfc6b4c8d479562edb620c6a237f524ff
Jyota <jyotas@my.ccsu.edu>
2013-09-11 01:16:44 -0700

    add modified bagging procedure

---
+++ modifiedBagging.R
@@ -0,0 +1,55 @@
+modifiedBagging <- function(x, y, rep=1000, proportion=0.632, start="random",stopP,stopT2)
+{
+# Implements modified bagging schema to obtain estimates related to feature selection
+# x is data frame of independent variables
+# y is data frame dependent variables
+# rep is number of random samples/LDA classifiers to obtain estimates with
+# start, stopP, stopT2 are used for hybridFeatureSelection procedure run prior to checking each classifier on Out of Bag samples
+# proportion is proportion of x to be in bag, rest will be out of bag
+  source("hybridFeatureSelection.R")
+  require(MASS)
+  repStats = as.data.frame(matrix(ncol=3,nrow=rep))
+  colnames(repStats) = c("Accuracy","Sensitivity","Specificity")
+  varsStats = as.data.frame(matrix(ncol=3, nrow=ncol(x)))
+  colnames(varsStats) = c("Variable", "Times_Selected","Perc_Selected")
+  varsStats[,1] = as.matrix(colnames(x))
+  varsStats[,2] = 0.0
+  varsStats[,3] = 0.0
+
+  for(j in 1:rep){
+      # for now, handles only two class problem
+      inBagJ1 = data.frame(x,y=y, check.names=FALSE)
+      rownames(inBagJ1) = rownames(x)
+      inBagJ1 = inBagJ1[inBagJ1$y==0,]      
+      inBagJ2 = data.frame(x,y=y, check.names=FALSE)
+      rownames(inBagJ2) = rownames(x)
+      inBagJ2 = inBagJ2[inBagJ2$y==1,]
+      inBagJ1 = inBagJ1[sample(nrow(inBagJ1),size=round(nrow(inBagJ1)*proportion,0),replace=FALSE),]
+      inBagJ2 = inBagJ2[sample(nrow(inBagJ2),size=round(nrow(inBagJ2)*proportion,0),replace=FALSE),]      
+      fullInBag = rbind(inBagJ1,inBagJ2)
+      # Need to ensure there will not be a inverse singularity below
+      fullInBag = fullInBag[,which(round(colSums(fullInBag),0)!=0)]
+      OOBJ1   = data.frame(x,y=y, check.names=FALSE)
+      rownames(OOBJ1) = rownames(x)
+      OOBJ1   = OOBJ1[OOBJ1$y==0,]
+      OOBJ2   = data.frame(x,y=y, check.names=FALSE)
+      rownames(OOBJ2) = rownames(x)
+      OOBJ2   = OOBJ2[OOBJ2$y==1,]
+      OOBJ1   = subset(OOBJ1, !(rownames(OOBJ1) %in% rownames(inBagJ1)))
+      OOBJ2   = subset(OOBJ2, !(rownames(OOBJ2) %in% rownames(inBagJ2)))
+      fullOOB = rbind(OOBJ1, OOBJ2)
+      #fullOOB = fullOOB[,which(round(colSums(fullOOB),0)!=0]
+      cat("Beginning feature selection-- run #: ", j, " in bag samples: ", nrow(fullInBag), " OOB samples: ", nrow(fullOOB), "\n")
+      tmpDat = hybridFeatureSelection(as.matrix(fullInBag[,1:(NCOL(fullInBag)-1)]),as.matrix(fullInBag[,NCOL(fullInBag)]),start,stopP,stopT2) 
+      cat("Beginning LDA fit-- run #: ", j, " ")
+      tmpFit = lda(classes ~ .,data=data.frame(tmpDat,classes=fullInBag[,NCOL(fullInBag)],check.names=FALSE))
+      q = cbind(as.factor(fullOOB$y),predict(tmpFit,fullOOB)$class)
+      repStats[j,1] = NROW(q[q[,1]==q[,2],])/NROW(fullOOB)
+      repStats[j,2] = NROW(q[q[,1]==q[,2] & q[,1]==2,])/NROW(fullOOB[fullOOB$y==1])
+      repStats[j,3] = NROW(q[q[,1]==q[,2] & q[,1]==1,])/NROW(fullOOB[fullOOB$y==0])
+      cat("result in accuracy: ", repStats[j,1], " sensitivity: ", repStats[j,2], " specificity: ", repStats[j,3],"\n")
+      varsStats[varsStats$Variable %in% colnames(tmpDat),]$Times_Selected = varsStats[varsStats$Variable %in% colnames(tmpDat),]$Times_Selected + 1
+  }
+  varsStats$Perc_Selected = varsStats$Times_Selected / sum(varsStats$Times_Selected)
+  return(list(varsStats=varsStats,repStats=repStats))
+}
