31e0c2b51554ba859800ba37e6f09c1b3fa3f93d
Jyota <jyotas@my.ccsu.edu>
2013-11-28 11:47:57 -0800

    Add iterative procedure to generate sequence of biomarkers

---
+++ findInformative.R
@@ -0,0 +1,17 @@
+# Generate a sequence of alternative biomarkers, where variables used in previous iteration
+# are removed from pool of possible variables, for a given P variables.
+
+findInformative <- function(x, y, rep=100, proportion=0.632, start="random",stopP,stopT2,priors=NULL){
+  returnMatrix = matrix(nrow=rep,ncol=(5+stopP))
+  result = modifiedBagging(x,y,rep=1,proportion=proportion,start=start,stopP=stopP,stopT2=stopT2,priors=priors)
+  finals = result$varsStats[result$varsStats[,2]>0,1]  
+  returnMatrix[1,] = c(1,result$repStats[1,1],result$repStats[1,2],result$repStats[1,3],result$repStats[1,4], t(finals))
+  for(i in 2:rep){    
+   result = modifiedBagging(x[,-which(colnames(x) %in% finals)],y,rep=1,proportion=proportion,start=start,stopP=stopP,stopT2=stopT2,priors=priors)
+   finals = c(result$varsStats[result$varsStats[,2]>0,1],finals)
+   returnMatrix[i,] = c(i,result$repStats[1,1],result$repStats[1,2],result$repStats[1,3],result$repStats[1,4], t(result$varsStats[result$varsStats[,2]>0,1]))
+  }
+  returnMatrix = as.data.frame(returnMatrix)
+  colnames(returnMatrix) <- c('Index','Accuracy','Sensitivity','Specificity','T2',paste('V',seq(1:stopP),sep=""))
+  return(returnMatrix)
+}
